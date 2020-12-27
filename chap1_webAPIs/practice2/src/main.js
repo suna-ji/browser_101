c96faec9dae000596820a963014b8c860d28abe5
지선아 <jisuna314@gmail.com>
2020-12-27 22:13:52 +0900

    chap1_prac2_solution

--- chap1_webAPIs/practice2/src/main.js
+++ chap1_webAPIs/practice2/src/main.js
@@ -1,16 +1,12 @@
-function getXY(event){
-    console.log(`client: ${event.clientX} ,${event.clientY}
-    page: ${event.pageX}, ${event.pageY}`);
-}
-
-function changeColor(event){
-    event.target.style.backgroundColor = "green";
-}
-
 function setEventListener(){
     const divs = document.querySelector('.divs');
-    divs.addEventListener('click', event => getXY(event));
-    divs.addEventListener('click', event => changeColor(event));
+    divs.addEventListener('click', event => {
+        const rect = event.target.getBoundingClientRect();
+        console.log(rect);
+        console.log(`client: ${event.clientX} ,${event.clientY}`);
+        console.log(`page: ${event.pageX}, ${event.pageY}`);
+        
+    })
 }
 
 setEventListener();
