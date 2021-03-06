7e4285aff335663a68e4cc4b417689af39d36342
suna-ji <jisuna314@gmail.com>
2021-03-06 21:48:02 +0900

    Modify

--- chap1_webAPIs/practice5/src/main.js
+++ chap1_webAPIs/practice5/src/main.js
@@ -2,7 +2,8 @@ const find_box = document.querySelector('.find_box');
 const rabbit = document.querySelector('.rabbit');
 
 find_box.addEventListener('click', (event) => {
-    var clientRect = rabbit.getBoundingClientRect();
-    window.scrollTo(clientRect.x, clientRect.y);
+    // var clientRect = rabbit.getBoundingClientRect();
+    // window.scrollTo(clientRect.x, clientRect.y);
+    rabbit.scrollIntoView({behavior: 'smooth', block: 'center'});
 })
 
