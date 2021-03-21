bd7de162c8eb51b4214d35a94cdcf4b956efc2eb
suna-ji <jisuna314@gmail.com>
2021-03-21 21:42:18 +0900

    Implement input

---
+++ chap2_DOM/practice1/src/main.js
@@ -0,0 +1,31 @@
+const input = document.querySelector('.item_input');
+let items = [];
+
+input.addEventListener('keyup', (event) => {
+    if(event.key == 'Enter'){
+        const item_name = input.value;
+        if(item_name != ""){
+            items.push(item_name);
+            input.value = "";
+            displayItems(items);
+        }
+    }else{
+
+    }
+    console.log(items);
+})
+
+function displayItems(items) {
+    const shopping_list = document.querySelector('.shopping_list');
+    shopping_list.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+function createHTMLString(item) {
+    return `
+        <div class = "item">
+            <p class = "item_name">${item}</p>
+            <button class = "trashBtn"><img src="imgs/trash.png" alt="trash" class = "trashBtn_img"/></button>
+        </div>
+    `;
+}
+
