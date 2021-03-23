03a9d9af02dbd677076e062ac9af52f00bcbaad9
suna-ji <jisuna314@gmail.com>
2021-03-23 21:50:35 +0900

    .

--- chap2_DOM/practice1_solution/main.js
+++ chap2_DOM/practice1_solution/main.js
@@ -19,38 +19,29 @@ function onAdd(){
     input.value = '';
     input.focus();
 }
-
+let id = 0; // UUID쓰는게 좋긴함!
 function creatItem(text) {
     const itemRow = document.createElement('li');
     itemRow.setAttribute('class', 'item__row');
-
-    const item = document.createElement('div');
-    item.setAttribute('class', 'item');
-
-    const name = document.createElement('span');
-    name.setAttribute('class', 'item__name');
-    name.innerText = text;
-    
-    const deleteBtn = document.createElement('button');
-    deleteBtn.setAttribute('class', 'item__delete');
-    deleteBtn.innerHTML = '<i class = "fas fa-trash-alt"></i>';
-    
-    const itemDivider = document.createElement('div');
-    itemDivider.setAttribute('class', 'item__divider');
-
-    item.appendChild(name);
-    item.appendChild(deleteBtn);  
-    
-    itemRow.appendChild(item);
-    itemRow.appendChild(itemDivider);
+    itemRow.setAttribute('data-id', id);
+    itemRow.innerHTML = `
+            <div class="item">
+                <span class="item__name">${text}</span>
+                <button class="item__delete">
+                    <i class = "fas fa-trash-alt" data-id = ${id}></i>
+                </button>
+            </div>
+            <div class="item__divider"></div>`;
+    id++;
     return itemRow;
 
 }
 
 items.addEventListener('click', (event) => {
-    if(event.target.className == 'item__delete' || event.target.className === "fas fa-trash-alt"){
-        console.log(event.path[3]);
-        items.removeChild(event.path[3]);
+    const id = event.target.dataset.id;
+    if(id){
+        const toBeDeleted = document.querySelector(`.item__row[data-id = "${id}"]`);
+        toBeDeleted.remove();
     }
 });
 
